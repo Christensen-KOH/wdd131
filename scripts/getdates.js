8badba904c78f369241d089a281811211ce708d2
Christensen KOH <kkce1996@gmail.com>
2026-03-09 04:31:21 +0300

    Dynamic 1

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+const currentYear = new Date().getFullYear();
+
+document.getElementById("current-year").textContent = currentYear;
+
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
